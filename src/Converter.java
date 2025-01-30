c9543a24539e5590a8cdbdd62fb44ae700aba5c6
Nikhil Khatwani <85192316+nikhilkhatwani@users.noreply.github.com>
2025-01-30 22:25:35 +0000

    initial commit

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,19 @@
 
+public class Converter {
+    public static void main(String[] args) {
+        String strFloat = "374.25";
+        float from_strFloat = Float.parseFloat(strFloat);
+
+        String strInt = "1250";
+        int from_strInt = Integer.parseInt(strInt);
+
+        float floatNum = 56.715f;
+        int from_floatNum = (int) floatNum;
+
+        int intNum = 1500;
+        float from_intNum = (float) intNum;
+
+        double doubleNum = 3.14159265;
+        String from_doubleNum = "" + doubleNum;
+    }
+}
